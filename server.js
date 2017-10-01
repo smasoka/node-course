f400694337c019217a6c2e810665b10846086c22
smasoka <sakhile.harvey@gmail.com>
2017-10-01 17:29:31 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+//Web frame work for node
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('View engine', 'hbs');
+
+
+// Register a middleware
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+//maintenance mode
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+   return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) =>{
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+  });
+});
+
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to fulfil this request!'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port: 3000');
+});
